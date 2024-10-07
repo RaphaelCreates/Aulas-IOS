3ae93b8acf6efa47112cd1750b2a0cac83401440
RaphaelCreates <raphael.oliveira3083@outlook.com>
2024-10-07 11:51:07 -0300

    salvando JS

---
+++ AulasJS/Aula01/main.js
@@ -0,0 +1,14 @@
+let nota01, nota02,nota03, media;
+nota01 = 2*0.6;
+nota02 = 3*0.4;
+media = (nota01 + nota02);
+console.log(media.toFixed(0));
+if(media >= 7){
+
+console.log("aprovado" + " sua media é " + media.toFixed(0));
+    
+}
+else{
+    console.log("reprovado")
+}
+
